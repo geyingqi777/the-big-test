986c1e47f008396fa5ee1e5f8edb6f29737810d1
asage <asage@welove-inc.com>
2019-01-02 18:35:14 +0800

    JavaInteger特性测试

---
+++ src/gyq/java/base/test/IntegerTest.java
@@ -0,0 +1,22 @@
+package gyq.java.base.test;
+
+/**
+ * Created by Asa on 2019/1/2.
+ */
+public class IntegerTest {
+	public static void main(String[] args) {
+		Integer a = new Integer(1);
+		int b = 1;
+		// 自动装箱
+		Integer c = b;
+
+		// 比较双方,存在int类型,自动拆箱了,结果为true
+		System.out.println(a == b);
+		// 比较双方都是integer对象,不会自动拆箱,比较内存地址,结果为false
+		System.out.println(a == c);
+		// 溢出测试
+		a = a + Integer.MAX_VALUE;
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(a);
+	}
+}
