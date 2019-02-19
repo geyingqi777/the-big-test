852cbbb295aadbd4c0c7d22f0d1d1b59d504468c
asage <asage@welove-inc.com>
2019-02-19 18:04:40 +0800

    leetcode字符串相乘

--- src/main/java/gyq/java/leetcode/_43/Solution.java
+++ src/main/java/gyq/java/leetcode/_43/Solution.java
@@ -2,6 +2,8 @@ package gyq.java.leetcode._43;
 
 import java.math.BigDecimal;
 
+import com.alibaba.fastjson.JSON;
+
 /**
  * 字符串相乘 Created by ge_yi on 2019/2/18.
  */
@@ -28,7 +30,8 @@ public class Solution {
 	 * @return
 	 */
 	public String multiply(String num1, String num2) {
-		// 这个题的解决思路是模拟小学乘法竖式的方式,
+		// 本方法的解决思路是完全模拟小学乘法竖式的方式, 完全还原且没有利用乘积位置的规律
+		
 		// 而且为了便于理解,总是把更短的那个数值放在竖式下面, 长的放在竖式上面
 		if ("0".equals(num1) || "0".equals(num2)) {
 			return "0";
@@ -52,14 +55,18 @@ public class Solution {
 			lengthShorter = length;
 		}
 		int lineCount = lengthShorter;// 行数
-		// 列数加1的原因: 每一次j循环完了之后,得到的数字位数可能为length2和length1中较大的再多一位
+		// 列数加1的原因: 每一次j循环完了之后,得到的数字位数可能为length2和length1中较大的再多1位,
+		// 因为两个个位数相乘最大是81 再加上前面最多进也小于10, 合起来是还是2位数, 循环到了最前一位, 只多出1位数字
 		int rowCount = lengthLonger + 1;// 列数
 		// lineCount行rowCount列元素的二维数组
 		int[][] array = new int[lineCount][rowCount];
+
+		// 注: 此处可以不用二维数组, 用一个字符串数组也可以,比二维数组更容易理解
+
 		// 外层循环,在小学乘法竖式中,放在下面
 		for (int i = lengthShorter - 1; i >= 0; i--) {
+			// 因为两个个位数相乘最大是81 再加上前面最多进9, 合起来是90, 循环到了最前一位, 只多出1位数字, 所以这里需要保留到10位
 			int position10 = 0;
-			int position100 = 0;
 			// 内层循环,在乘法竖式中,放在上面
 			for (int j = lengthLonger - 1; j >= 0; j--) {
 				char c = numLonger.charAt(j);
@@ -68,29 +75,35 @@ public class Solution {
 				char c1 = numShorter.charAt(i);
 				// 得到该位的int数值
 				int n2 = c1 - '0';
-				int multiply = n1 * n2 + position10;
+				// 这里注意:上一次进位的十位,下一次加到个位,百位加到十位, 千位加到百位
+				int multiply = n1 * n2 + position10 * 1;
 				// 得到个位数
 				int positon1 = multiply % 10;
 				array[lengthShorter - 1 - i][j + 1] = positon1;
 				// 得到十位数
 				position10 = multiply % 100 / 10;
-
 			}
 			// 最后得到该行的首位
 			array[lengthShorter - 1 - i][0] = position10;
 		}
 		// for (int[] ints : array) {
-		// // 打印一行看下
-		// System.out.println(JSON.toJSONString(ints));
+		// 	// 打印一行看下
+		// 	System.out.println(JSON.toJSONString(ints));
 		// }
 
 		// 最后把二维数组按列相加, 此处还是不能用int直接相加,可能会溢出
 		// 外层循环是列
 		StringBuilder stringBuilder = new StringBuilder();
-		int position10 = 0;
-		int position100 = 0;
-		int position1000 = 0;
+		int position10 = 0;// 十位
+		int position100 = 0;// 百位
+		// 题目限定了num1 和 num2 的长度小于110, 所以竖式中,一列最大为110个9相加, 为990, 前面进位过来的数可能大于10, 合起来大于1000时,向前进位可能大于100
+		// 所以这里需要保留到千位
+		int position1000 = 0;// 千位
 		for (int i = 0; i < rowCount + lineCount; i++) {
+			StringBuilder stringBuilder1 = new StringBuilder("5023045302335819");
+			if (stringBuilder1.reverse().toString().equals(stringBuilder.toString())) {
+				System.out.println("错误调试时从后往前找到没错的那一位,debug用");
+			}
 			int sum = 0;
 			// 内层循环是行
 			for (int j = 0; j < lineCount; j++) {
@@ -103,7 +116,8 @@ public class Solution {
 					}
 				}
 			}
-			sum = sum + position100 + position10;
+			// 这里注意:上一次进位的十位,下一次加到个位,百位加到十位, 千位加到百位
+			sum = sum + position10 * 1 + position100 * 10 + position1000 * 100;
 			// 这一列加完了
 			// 得到个位数
 			int positon1 = sum % 10;
@@ -127,14 +141,76 @@ public class Solution {
 		return s;
 	}
 
+	/**
+	 * 另一种答案
+	 * 
+	 * @param num1
+	 * @param num2
+	 * @return
+	 */
+	public String multiply2(String num1, String num2) {
+		// 先把string翻转
+		String n1 = new StringBuilder(num1).reverse().toString();
+		String n2 = new StringBuilder(num2).reverse().toString();
+
+		// 两个数值的乘积, 位数不会大于两个数值的位数之和
+		int[] d = new int[n1.length() + n2.length()]; // 构建数组存放乘积
+		for (int i = 0; i < n1.length(); i++) {
+			for (int j = 0; j < n2.length(); j++) {
+				// 这个算法,了解乘积位置的规律最重要
+				// 乘积位置的规律: 乘法运算的时候每一个都要相乘也就是n*n
+				// 当被乘数到达第二位的时候乘以乘数的第一位就要写到第二位
+				// 由此得到结果摆放的位置按i+j
+				d[i + j] += (n1.charAt(i) - '0') * (n2.charAt(j) - '0'); // 在正确位置累加乘积
+			}
+		}
+
+		StringBuilder sb = new StringBuilder();
+		for (int i = 0; i < d.length; i++) {
+			int digit = d[i] % 10; // 当前位
+			int carry = d[i] / 10; // 进位
+			if (i + 1 < d.length) {
+				d[i + 1] += carry;
+			}
+			sb.insert(0, digit); // prepend
+		}
+		// 移除前导零
+		while (sb.charAt(0) == '0' && sb.length() > 1) {
+			sb.deleteCharAt(0);
+		}
+		return sb.toString();
+	}
+
 	public static void main(String[] args) {
 		Solution solution = new Solution();
 		String s2 = "401716832807512840963";
-		String s1 = "167141802233061013023557397451289113296441069";
+		String s1 = "23557397451289113";
 		String multiply = solution.multiply(s1, s2);
 		System.out.println(multiply);
 		BigDecimal bigDecimal = new BigDecimal(s1);
 		BigDecimal bigDecimal1 = new BigDecimal(s2);
-		System.out.println(bigDecimal.multiply(bigDecimal1).toString());
+		String string = bigDecimal.multiply(bigDecimal1).toString();
+		System.out.println(string);
+		System.out.println(string.equalsIgnoreCase(multiply));
+
+		// long begin = System.currentTimeMillis();
+		// for (int i = 10000000; i < 20000000; i++) {
+		// for (int j = 10000000; j < 20000000; j++) {
+		// String ss1 = String.valueOf(i);
+		// String ss2 = String.valueOf(j);
+		// BigDecimal bigDecimal3 = new BigDecimal(ss1);
+		// BigDecimal bigDecimal4 = new BigDecimal(ss2);
+		// String string33 = bigDecimal3.multiply(bigDecimal4).toString();
+		// String multiply = solution.multiply(ss1, ss2);
+		// boolean b = string33.equalsIgnoreCase(multiply);
+		// if (!b) {
+		// // 不相同
+		// System.out.println("wrong");
+		// System.out.println(String.format("s1 %s s2 %s", ss1, ss2));
+		// }
+		// }
+		// System.out.println(String.format("执行到 %s", i));
+		// }
+		// System.out.println(String.format("finish %s", (System.currentTimeMillis() - begin) / 1000 / 60));
 	}
 }
