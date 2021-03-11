0e9cc07336ee23f3f537c6cc75fcba1b610f8149
geyingqi <geyingqi@shuidi-inc.com>
2021-03-11 15:31:01 +0800

    feat: leetcode算法刷题

--- src/main/java/gyq/java/algorithm/leetcode/p111_minimum_depth_of_binary_tree/Solution.java
+++ src/main/java/gyq/java/algorithm/leetcode/p111_minimum_depth_of_binary_tree/Solution.java
@@ -2,6 +2,9 @@ package gyq.java.algorithm.leetcode.p111_minimum_depth_of_binary_tree;
 
 import gyq.java.common_data_struct.TreeNode;
 
+import java.util.LinkedList;
+import java.util.Queue;
+
 /**
  * 二叉树最小深度
  *
@@ -10,7 +13,14 @@ import gyq.java.common_data_struct.TreeNode;
  */
 
 class Solution {
-    public int minDepth(TreeNode root) {
+
+    /**
+     * DFS解决方法
+     *
+     * @param root
+     * @return
+     */
+    public int minDepth2(TreeNode root) {
         if (root == null) {
             return 0;
         }
@@ -27,6 +37,40 @@ class Solution {
             // 没有子节点
             return 1;
         }
+    }
+
+    /**
+     * 2021/3/11 BFS解决方法
+     */
+    public int minDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        Queue<TreeNode> current = new LinkedList<>();
+        current.offer(root);
+        int finalStep = 1;
+        // 遍历这一层中所有的节点
+        while (!current.isEmpty()) {
+            // 得到当前的size,从队列中取出当前size个元素
+            int size = current.size();
+            for (int i = 0; i < size; i++) {
+                TreeNode poll = current.poll();
+                if (poll.left == null && poll.right == null) {
+                    // 到达一个叶子节点,没有子树了, 已经找到最小深度
+                    return finalStep;
+                }
+                // 下一层节点放到队列中
+                if (poll.left != null) {
+                    current.offer(poll.left);
+                }
+                if (poll.right != null) {
+                    current.offer(poll.right);
+                }
+            }
+            // 向下一层,步数+1
+            finalStep++;
+        }
+        return finalStep;
     }
 }
 
