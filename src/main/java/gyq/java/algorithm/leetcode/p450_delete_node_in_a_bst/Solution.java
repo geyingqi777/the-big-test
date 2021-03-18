91b0055c466c0b9dc7e8f7772bebc1c90c5e5346
geyingqi <geyingqi@shuidi-inc.com>
2021-03-18 11:25:31 +0800

    feat: 删除二叉搜索树中的节点

---
+++ src/main/java/gyq/java/algorithm/leetcode/p450_delete_node_in_a_bst/Solution.java
@@ -0,0 +1,17 @@
+package gyq.java.algorithm.leetcode.p450_delete_node_in_a_bst;
+
+import gyq.java.common_data_struct.TreeNode;
+
+/**
+ * 450. 删除二叉搜索树中的节点
+ *
+ * @see https://leetcode-cn.com/problems/delete-node-in-a-bst/
+ */
+class Solution {
+    public TreeNode deleteNode(TreeNode root, int key) {
+        // 遍历找到指定key的节点
+        
+        // 难点在于删除之后的平衡调整,需要懂的基本概念: 前驱节点, 后继节点
+    }
+    
+}
